ca00cbf5c09e89f788de61b479b8ece91af18ce4
willoughby_hood <willoughbyhood29@googlemail.com>
2021-04-23 14:11:16 +0100

    exercises

---
+++ exercises/directory-ex7.rb
@@ -0,0 +1,47 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "Enter the cohort"
+    cohort = gets.chomp.downcase.to_sym
+    loop do
+      if cohort == :january || cohort == :february || cohort == :march || cohort == :april || cohort == :may || cohort == :june || cohort == :july || cohort == :august || cohort == :september || cohort == :october || cohort == :november || cohort == :december
+        break
+      else
+        puts "Please reenter the cohort month"
+        cohort = gets.chomp.downcase.to_sym
+      end
+    end
+    # add the student hash to the array
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
